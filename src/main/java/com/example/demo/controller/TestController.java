73dc2c29df1915f0fafbd7085220a37e2a805de3
liujinfa <liujinfa9242@dingtalk.com>
2021-12-29 16:12:10 +0800

    add api

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -15,6 +17,15 @@ public class TestController {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("1", 1);
         jsonObject.put("test", test);
+        return jsonObject;
+    }
+
+    @GetMapping("/demo2")
+    public JSONObject demo2(String test) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("1", 1);
+        jsonObject.put("test", test);
+        log.info("test: {}", test);
         return jsonObject;
     }
 
