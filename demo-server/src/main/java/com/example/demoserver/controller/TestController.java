7d4e3785d5aecd0d8cea3d53c41d03b692e72e1b
Fa <183461750@qq.com>
2022-11-15 20:53:23 +0800

    fix: 单点登录调整

--- demo-server/src/main/java/com/example/demoserver/controller/TestController.java
+++ demo-server/src/main/java/com/example/demoserver/controller/TestController.java
@@ -1,10 +1,15 @@
 package com.example.demoserver.controller;
 
+import cn.dev33.satoken.context.SaHolder;
+import cn.dev33.satoken.sso.SaSsoUtil;
 import com.alibaba.fastjson.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -29,5 +34,20 @@ public class TestController {
         return jsonObject;
     }
 
+
+
+    // 获取指定用户的关注列表
+    @RequestMapping("/sso/getFollowList")
+    public Object ssoRequest(Long loginId) {
+
+        // 校验签名，签名不通过直接抛出异常
+        SaSsoUtil.checkSign(SaHolder.getRequest());
+
+        // 查询数据 (此处仅做模拟)
+        List<Integer> list = Arrays.asList(10041, 10042, 10043, 10044);
+
+        // 返回
+        return list;
+    }
 
 }
