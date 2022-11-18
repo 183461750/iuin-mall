6c54375e2d4064462c84516b049724b0b79b0c01
Fa <183461750@qq.com>
2022-11-18 20:43:18 +0800

    fix: 修复无法构建的问题

---
+++ test-sso/test-sso-common/src/main/java/com/iuin/testssocommon/TestSsoCommonApplication.java
@@ -0,0 +1,13 @@
+package com.iuin.testssocommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSsoCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSsoCommonApplication.class, args);
+    }
+
+}
---
+++ test-sso/test-sso-common/src/test/java/com/iuin/testssocommon/TestSsoCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuin.testssocommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSsoCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
