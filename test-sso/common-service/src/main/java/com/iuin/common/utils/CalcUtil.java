34539fe9ae832c14ee2447f1d84c80a38b88d538
刘金发 <liujinfa9242@dingtalk.com>
2023-03-20 21:02:33 +0800

    test: 测试集合流字符串拼接

---
+++ test-sso/common-service/src/main/java/com/iuin/common/utils/CalcUtil.java
@@ -0,0 +1,32 @@
+package com.iuin.common.utils;
+
+import java.util.Optional;
+
+/**
+ * 计算相关工具类
+ * @author fa
+ */
+public class CalcUtil {
+
+    /**
+     * 两数相加
+     */
+    public static Integer add(Integer a, Integer b) {
+        return Optional.ofNullable(a).orElse(0) + Optional.ofNullable(b).orElse(0);
+    }
+
+    /**
+     * 两数相减
+     */
+    public static Integer sub(Integer a, Integer b) {
+        return Optional.ofNullable(a).orElse(0) - Optional.ofNullable(b).orElse(0);
+    }
+
+    /**
+     * 比较两数大小，取较大值
+     */
+    public static Integer max(Integer a, Integer b) {
+        return Math.max(Optional.ofNullable(a).orElse(0), Optional.ofNullable(b).orElse(0));
+    }
+
+}
---
+++ test-sso/sso-server-service/src/test/java/com/iuin/ssoserver/maintest/test6/TestMain.java
@@ -0,0 +1,109 @@
+package com.iuin.ssoserver.maintest.test6;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.iuin.common.utils.CalcUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * <p>TODO</p>
+ *
+ * @author fa
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        String jsonStr = "[{\"skuCode\":\"75117\",\"receivedQty\":1,\"inventorySts\":\"AVAILABLE\"},{\"skuCode\":\"75117\",\"receivedQty\":1,\"inventorySts\":\"NOT_AVAILABLE\"}]";
+        String omsHandleResult = toOmsHandleResult(JSONUtil.toList(jsonStr, OmsDeliveryGoodsDetailBO.class));
+
+        System.out.println(omsHandleResult);
+    }
+
+    public static String toOmsHandleResult(List<OmsDeliveryGoodsDetailBO> detailBOList) {
+        detailBOList = Optional.ofNullable(detailBOList).orElseGet(ArrayList::new);
+        Map<String, Integer> inventoryStsReceivedQtyMap = detailBOList.stream().filter(detailBO -> InventoryStsEnum.AVAILABLE.getCode().equals(detailBO.getInventorySts())).collect(Collectors.toMap(OmsDeliveryGoodsDetailBO::getInventorySts, OmsDeliveryGoodsDetailBO::getReceivedQty, CalcUtil::add));
+        inventoryStsReceivedQtyMap.put(InventoryStsEnum.NOT_AVAILABLE.getCode(), detailBOList.stream().filter(detailBO -> !InventoryStsEnum.AVAILABLE.getCode().equals(detailBO.getInventorySts())).map(OmsDeliveryGoodsDetailBO::getReceivedQty).reduce(CalcUtil::add).orElse(0));
+        return inventoryStsReceivedQtyMap.keySet().stream().filter(inventorySts -> Optional.ofNullable(inventoryStsReceivedQtyMap.get(inventorySts)).orElse(0) > 0).sorted(Comparator.comparing(inventorySts -> InventoryStsEnum.getEnum(inventorySts).ordinal())).map(inventorySts -> InventoryStsEnum.getName(inventorySts) + ": " + inventoryStsReceivedQtyMap.getOrDefault(inventorySts, 0)).reduce((s, s1) -> StrUtil.join(", ", s, s1)).orElse("");
+    }
+
+    /**
+     * oms商品发货详情
+     * @author fa
+     */
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Accessors(chain = true)
+    public static class OmsDeliveryGoodsDetailBO {
+
+        /**
+         * SKU 编码
+         */
+        private String skuCode;
+
+        /**
+         * 库存状态
+         */
+        private String inventorySts;
+
+        /**
+         * 入库数量
+         */
+        private Integer receivedQty;
+
+    }
+
+    @Getter
+    public enum InventoryStsEnum {
+
+        /**
+         * OMS名称: 可售
+         */
+        AVAILABLE("AVAILABLE", "可售"),
+        /**
+         * OMS名称: 残次
+         */
+        NOT_AVAILABLE("NOT_AVAILABLE", "不可售"),
+        /**
+         * OMS名称: 冻结
+         */
+        FROZEN("FROZEN", "冻结"),
+        /**
+         * OMS名称: 临期
+         */
+        ADVENT("ADVENT", "临期"),
+        /**
+         * OMS名称: 过期
+         */
+        INVALID("INVALID", "过期"),
+
+        UNKNOWN("UNKNOWN", "未知"),
+
+        ;
+
+        private final String code;
+        private final String name;
+
+        InventoryStsEnum(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public static String getName(String code) {
+            return Arrays.stream(InventoryStsEnum.values()).filter(o -> o.getCode().equals(code)).map(InventoryStsEnum::getName).findFirst().orElse("");
+        }
+
+        public static InventoryStsEnum getEnum(String code) {
+            return Arrays.stream(InventoryStsEnum.values()).filter(o -> o.getCode().equals(code)).findFirst().orElse(UNKNOWN);
+        }
+
+    }
+
+}
