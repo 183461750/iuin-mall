4e4a7be58bd8932d6661a703dc663c2fe42c69f2
刘金发 <liujinfa9242@dingtalk.com>
2024-01-02 14:54:38 +0800

    feat(mp_ui): 添加模块

---
+++ test-sso/mp-ui-service/src/main/java/com/iuin/mp_ui/MpUiServiceApplication.java
@@ -0,0 +1,16 @@
+package com.iuin.mp_ui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fa
+ */
+@SpringBootApplication
+public class MpUiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpUiServiceApplication.class, args);
+    }
+
+}
---
+++ test-sso/mp-ui-service/src/test/java/com/iuin/mp_ui/MpUiServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.iuin.mp_ui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpUiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
