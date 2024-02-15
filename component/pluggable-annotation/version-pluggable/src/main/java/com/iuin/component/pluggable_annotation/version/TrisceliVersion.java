f2cd1e8108b0b03bc0f5b503ea13fd5487062c93
刘金发 <liujinfa9242@dingtalk.com>
2024-02-16 05:50:55 +0800

    fix(version_processor): 调整代码逻辑

--- component/pluggable-annotation/version-pluggable/src/main/java/com/iuin/component/pluggable_annotation/version/TrisceliVersion.java
+++ component/pluggable-annotation/version-pluggable/src/main/java/com/iuin/component/pluggable_annotation/version/TrisceliVersion.java
@@ -1,9 +1,11 @@
 package com.iuin.component.pluggable_annotation.version;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-@Documented
-@Retention(RetentionPolicy.SOURCE) //只在编译期有效，最终不会打进class文件中
-@Target({ElementType.FIELD}) //仅允许作用于类属性之上
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.FIELD})
 public @interface TrisceliVersion {
 }
