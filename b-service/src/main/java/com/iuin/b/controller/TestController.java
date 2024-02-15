14ebeb67b90c35fec207da123bae86f020d372dd
刘金发 <liujinfa9242@dingtalk.com>
2024-02-16 05:49:09 +0800

    fix(b-service): 修复不存在的方法导致编译报错的问题

--- b-service/src/main/java/com/iuin/b/controller/TestController.java
+++ b-service/src/main/java/com/iuin/b/controller/TestController.java
@@ -1,10 +1,14 @@
 package com.iuin.b.controller;
 
+import cn.dev33.satoken.sign.SaSignUtil;
 import cn.dev33.satoken.sso.SaSsoUtil;
 import cn.dev33.satoken.stp.StpUtil;
 import cn.dev33.satoken.util.SaResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -13,8 +17,19 @@ public class TestController {
     // 查询我的账号信息
     @RequestMapping("/sso/myFollowList")
     public Object myFollowList() {
-        // 组织url，加上签名参数
-        String url = SaSsoUtil.addSignParams("http://127.0.0.1:9000/test/sso/getFollowList", StpUtil.getLoginId());
+        // 请求地址
+        String url = "http://127.0.0.1:9000/test/sso/getFollowList";
+        // 请求参数
+        Map<String, Object> paramMap = new LinkedHashMap<>();
+        paramMap.put("loginId", StpUtil.getLoginId());
+        paramMap.put("userId", 10001);
+        paramMap.put("money", 1000);
+
+        // 补全 timestamp、nonce、sign 参数，并序列化为 kv 字符串
+        String paramStr = SaSignUtil.addSignParamsAndJoin(paramMap);
+
+        // 将参数字符串拼接在请求地址后面
+        url += "?" + paramStr;
 
         // 调用，并返回 SaResult 结果
         SaResult res = SaSsoUtil.request(url);
