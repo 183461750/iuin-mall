41283557c8ca2c7c92690e17002bad7a2f400742
刘金发 <liujinfa9242@dingtalk.com>
2025-08-22 11:36:04 +0800

    feat(mcp-service): 为 McpTestFeignServiceImpl 类添加日志记录
    - 在 McpTestFeignServiceImpl 类中添加 log.info 日志记录
    - 记录 feign 中传递的 name 参数值

--- mcp-service/src/main/java/com/iuin/mcp/service/impl/McpTestFeignServiceImpl.java
+++ mcp-service/src/main/java/com/iuin/mcp/service/impl/McpTestFeignServiceImpl.java
@@ -1,16 +1,19 @@
 package com.iuin.mcp.service.impl;
 
 import com.iuin.mcp.service.IMcpTestFeignService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /**
  * @author fa
  */
+@Slf4j
 @Service
 public class McpTestFeignServiceImpl implements IMcpTestFeignService {
 
     @Override
     public String findIdByName(String name) {
+        log.info("feign 中传递的 name 参数值为:[{}]", name);
         return "xxx";
     }
 
