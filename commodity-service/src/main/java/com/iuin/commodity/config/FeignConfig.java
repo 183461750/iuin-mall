346248b4daaac5322db23bf7ee42ba4a7da71594
刘金发 <liujinfa9242@dingtalk.com>
2025-11-24 12:29:06 +0800

    refactor(config): 移除版本拦截器导入配置
    - 删除了对VersionRequestInterceptor的导入声明
    - 移除了@Import注解及相关配置
    - 添加了待办事项注释提醒后续处理

--- commodity-service/src/main/java/com/iuin/commodity/config/FeignConfig.java
+++ commodity-service/src/main/java/com/iuin/commodity/config/FeignConfig.java
@@ -1,16 +1,13 @@
 package com.iuin.commodity.config;
 
-import com.iuin.mall.component.nacos.interceptors.VersionRequestInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 /**
  * @author fa
  */
 @Configuration
-@Import({
-        VersionRequestInterceptor.class,
-})
 public class FeignConfig {
+
+    // TODO @Fa 添加nacos版本拦截器
 
 }
